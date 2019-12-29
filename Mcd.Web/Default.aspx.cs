6f15835c08c2159eb28dd39e04f5f9231bcfcaee
alex.soh <alex.soh@bertelsmann.de>
2019-12-30 05:50:32 +0800

    Add EntityFramework 6 into solution with configurations (part 1)

--- Mcd.Web/Default.aspx.cs
+++ Mcd.Web/Default.aspx.cs
@@ -1,13 +1,23 @@
 ﻿using System;
+using System.Linq;
 using System.Web.UI;
+using Mcd.Data;
 
 namespace Mcd.Web
 {
-    public partial class _Default : Page
+    public partial class Default : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                using (var ctx = new McdContext())
+                {
+                    var beverages = ctx.Beverages.ToList();
+                    Gridview1.DataSource = beverages;
+                    Gridview1.DataBind();
+                }
+            }
         }
     }
 }
